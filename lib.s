af79b5f6d5507a8436d1ef1c52a2d57097d1c3f6
Kishandharan <kishanrajeevi.sangamone@gmail.com>
2026-08-17 23:00:02 +0530

    Added a few more functions!!

--- lib.s
+++ lib.s
@@ -1,11 +1,11 @@
 .intel_syntax noprefix
 .global cfmt 
 .global exit 
+.global strlen 
+.global print 
 
 .section .text
-    # lea rdi, [dst]
-    # lea rsi, [src]
-    # mov dl, 'A'
+
 cfmt:
   cfmt_loop:
     mov al, [rsi]
@@ -19,22 +19,41 @@ cfmt:
 
     inc rsi 
     inc rdi 
-    inc rcx
     jmp cfmt_loop
 
   cfmt_handle_percent:
     mov [rdi], dl 
     inc rsi 
     inc rdi
-    inc rcx
     jmp cfmt_loop 
 
   cfmt_ret: 
-    mov rax, rcx
     ret 
 
 exit: 
   mov rax, 60
   xor rdi, rdi
   syscall
+  ret
 
+strlen:
+  strlen_loop:
+    cmp byte ptr [rdi], 0
+    je strlen_done 
+    inc rax
+    inc rdi
+    jmp strlen_loop
+
+  strlen_done: 
+    ret
+
+print: 
+  push rdi 
+  call strlen
+  push rax 
+  mov rax, 1 
+  mov rdi, 1 
+  pop rdx
+  pop rsi 
+  syscall
+  ret
